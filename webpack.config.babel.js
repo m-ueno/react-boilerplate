214a1d05020cb9ebaac5dfc9ae07af3933cb5209
masaru <masaru@twotop>
2016-07-28 19:33:12 +0900

    add devserver

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -12,6 +12,18 @@ const js = {
     publicPath: '',   // 間違えやすい。webpack-dev-server --content-baseで指定するので、ここでは変えない
     filename: 'bundle.js',
   },
+  devServer: {
+    // https://webpack.github.io/docs/webpack-dev-server.html#the-historyapifallback-option
+    // https://github.com/callemall/material-ui-webpack-example/blob/4dba4758a56756921e2ec75b352c41d1325a6241/webpack-dev-server.config.js
+    contentBase: './public',
+    hot: true,
+    inline: true,
+    port: 3000,
+    // If you are using the HTML5 history API you probably need to serve
+    // your index.html in place of 404 responses,
+    // which can be done by setting historyApiFallback: true
+    historyApiFallback: true,
+  },
   devtool: 'source-map',
   module: {
     preLoaders: [
