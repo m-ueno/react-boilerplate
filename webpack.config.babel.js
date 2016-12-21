3f809adfa4ba4081a5e84998796ede99ef1a2536
m-ueno <m-ueno@github.com>
2016-12-21 15:00:43 +0900

    del comment

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -9,7 +9,7 @@ const js = {
   entry: './index.jsx',
   output: {
     path: path.join(__dirname, 'public'),
-    publicPath: '',   // 間違えやすい。webpack-dev-server --content-baseで指定するので、ここでは変えない
+    publicPath: '',
     filename: 'bundle.js',
   },
   devServer: {
